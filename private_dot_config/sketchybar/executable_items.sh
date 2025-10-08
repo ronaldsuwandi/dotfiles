c4aee4d9826aebff6b6f8d623066d952c328d193
Ronald Suwandi <ronald@ronaldsuwandi.com>
2025-10-08 19:29:53 +0800

    updated sketchybar

--- private_dot_config/sketchybar/executable_items.sh
+++ private_dot_config/sketchybar/executable_items.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-
 source "$HOME/.config/sketchybar/variables.sh" # Loads all defined colors
-
-sketchybar --add event aerospace_workspace_change
 
 # Mission Control specifics using yabai
 SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
@@ -24,92 +21,33 @@ for i in "${!SPACE_ICONS[@]}"; do
   sketchybar --add space space."$sid" left --set space."$sid" "${space[@]}"
 done
 
-# aerospace workspace
-# for sid in $(aerospace list-workspaces --all); do
-#     sketchybar --add item space.$sid left \
-#         --subscribe space.$sid aerospace_workspace_change \
-#         --set space.$sid \
-#         label.padding_left=20 \
-# 		label.padding_right=20 \
-# 		icon.padding_left=0 \
-# 		icon.padding_right=0 \
-# 		background.padding_left=0 \
-# 		background.padding_right=0 \
-# 		background.color=$BAR_COLOR \
-#         label="$sid" \
-#         click_script="aerospace workspace $sid" \
-#         script="$CONFIG_DIR/plugins/aerospace.sh $sid"
-# done
-
 # frontapp
 COLOR="$WHITE"
 front_app_setting=(
   script="$PLUGIN_DIR/front_app.sh"
 	icon.drawing=off
+	background.drawing=true
 	background.height=20
-	background.padding_left=10
-	background.padding_right=10
-	background.border_width="$BORDER_WIDTH"
-	background.border_color="$COLOR"
-	background.corner_radius="$CORNER_RADIUS"
-	background.color="$BAR_COLOR"
-	label.color="$COLOR"
-	label.padding_left=10
-	label.padding_right=10
+	background.image.scale=0.8
 	associated_display=active
+	label.padding_left=30
 )
+
+
+# apps
+sketchybar --add item running_apps_updater right \
+  --set running_apps_updater script="plugins/list_apps.sh" \
+  --subscribe running_apps_updater space_change \
+  --subscribe running_apps_updater space_windows_change \
+
 
 # front app
 sketchybar --add item front_app center \
 	--set front_app "${front_app_setting[@]}" \
 	--subscribe front_app front_app_switched
+# sketchybar --set running_apps_updater script="plugins/front_app.sh"
+# 	--subscribe running_apps_updater front_app_switched
 
 # right spacer
 sketchybar --add item right_spacer right \
   --set right_spacer background.padding_right=40
-
-
-# list apps
-sketchybar --add item running_apps_updater right \
-  --set running_apps_updater script="plugins/list_apps.sh" \
-  --subscribe running_apps_updater space_change
-
-
-# sketchybar --set "space.$(aerospace list-workspaces --focused)" \
-#     label.background.color=$RED \
-#     label.color="$BLACK"
-
-# window_count=(
-#     label.padding_left=20
-#     label.padding_right=20
-#     label="⊞ $(aerospace list-windows --workspace focused --count)"
-#     label.background.height=$HEIGHT
-#     script="$CONFIG_DIR/plugins/aerospace.sh $sid"
-# )
-# sketchybar --add item window_count right \
-#     --set window_count "${window_count[@]}"
-
-# aerospace windows
-# windows=()
-# while IFS= read -r line; do
-#   windows+=("$line")
-# done < <(aerospace list-windows --workspace focused --format "%{window-id}|||%{app-name}")
-
-
-# for (( i=${#windows[@]}-1; i>=0; i-- )); do
-#     id=${windows[i]%|||*}
-#     name=${windows[i]#*|||}
-#     echo "window name=${name}"
-#     window=(
-#         label.padding_left=20
-#         label.padding_right=20
-#         label="$name"
-#         label.background.height=$HEIGHT
-#         click_script="aerospace focus --window-id $id"
-#         script="$CONFIG_DIR/plugins/aerospace.sh $sid"
-#     )
-#     sketchybar --add item window.${name// /-} right \
-#         --set window.${name// /-} "${window[@]}"
-# done
-# focused=$(aerospace list-windows --focused --format "%{app-name}")
-# sketchybar --set "window.${focused// /-}" label.background.color=$RED
--- private_dot_config/sketchybar/plugins/executable_front_app.sh
+++ private_dot_config/sketchybar/plugins/executable_front_app.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 source "$HOME/.config/sketchybar/variables.sh" # Loads all defined colors
 
-if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set "$NAME" label="$INFO" 
-    # --set /window.*/ label.background.color="$BAR_COLOR" \
-    # --set "window.${INFO// /-}" label.background.color="$RED"
-fi
+COLOR="$WHITE"
+app=(
+  background.height=20
+  background.border_width=2
+  background.corner_radius=5
+  background.border_color=0xffcdd6f4
+  background.color=0xff1a1b26
+  label.padding_left=10
+  label.padding_right=10
+  background.padding_left=7
+  background.padding_right=7
+  label.background.height=30
+  label.color=$COLOR
+)
 
+if [[ "$SENDER" = "front_app_switched" ]]; then
+  front_app="$INFO"
+  # Update center front_app label
+  sketchybar --set "$NAME" background.image="app.${front_app}" label="$front_app"
+fi
