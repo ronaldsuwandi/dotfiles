4bff5e32ab16f950d0b6a7924315383cc4bb24a3
Ronald Suwandi <ronald@ronaldsuwandi.com>
2026-06-04 18:58:34 +0800

    Code tidy up, fixed window state when changing space

--- private_dot_config/sketchybar/plugins/executable_front_app.sh
+++ private_dot_config/sketchybar/plugins/executable_front_app.sh
@@ -1,21 +1,6 @@
 #!/usr/bin/env bash
 source "$HOME/.config/sketchybar/variables.sh" # Loads all defined colors
 
-COLOR="$WHITE"
-app=(
-  background.height=20
-  background.border_width=2
-  background.corner_radius=5
-  background.border_color=0xffcdd6f4
-  background.color=0xff1a1b26
-  label.padding_left=10
-  label.padding_right=10
-  background.padding_left=7
-  background.padding_right=7
-  label.background.height=30
-  label.color=$COLOR
-)
-
 FOCUSED=$(yabai -m query --windows --window 2>/dev/null)
 ZOOMED=$(echo "$FOCUSED" | jq -r '.["has-fullscreen-zoom"]')
 FLOATED=$(echo "$FOCUSED" | jq -r '.["is-floating"]')
