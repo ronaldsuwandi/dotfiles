8f2d04fa6a37002baf5c84a1779fb45ce6de6bc1
Ronald Suwandi <ronald@ronaldsuwandi.com>
2026-07-11 14:52:53 +0800

    Rendered from main@ad298f1

--- .config/sketchybar/plugins/space_layout.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-LAYOUT=$(yabai -m query --spaces --space 2>/dev/null | jq -r '.type' 2>/dev/null)
-case "$LAYOUT" in
-  bsp)   sketchybar --set "$NAME" label="󰓫" label.drawing=on ;;
-  stack) sketchybar --set "$NAME" label="󰌨" label.drawing=on ;;
-  *)     sketchybar --set "$NAME" label.drawing=off ;;
-esac
--- .config/sketchybar/plugins/window_zoom_float_click.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-paneru send-cmd window manage
-# sketchybar --trigger yabai_zoom_change
-sketchybar --trigger paneru_manage_change
